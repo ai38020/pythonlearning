7f477565ae0ee50ca818a0d65de0c3d2afe00db4
ai38020 <419137691@qq.com>
2018-08-07 17:14:33 +0800

    get pv and uv

---
+++ linux command.py
@@ -0,0 +1,3 @@
+import os
+with open('userapi_access.log') as f:
+    print f.readline()
---
+++ nginxlog_PV_UV.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+
+ips = []
+with open('userapi_access.log') as f:
+    for line in f:
+        line.split( )
+        #ips.append(line.split ( ) [4])
+
+#print("PV is {0}".format(len (ips)))
+#print("UV is {0}".format(len (set (ips))))
+#print (set (ips))
+print (line)
+print (ips)
